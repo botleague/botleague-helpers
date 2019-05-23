a0f21e40d21bad2baad870fdbb4bf7628678be48
Craig Quiter <cquiter@gmail.com>
2019-05-23 13:24:22 -0700

    Avoid creating one giant document for simple key value store. Instead store in separate documents.

--- botleague_helpers/key_value_store.py
+++ botleague_helpers/key_value_store.py
@@ -3,14 +3,8 @@ from __future__ import print_function
 import botleague_helpers.constants as c
 
 
-DEFAULT_COLLECTION_NAME = 'simple_key_value_store'
-
-
 class SimpleKeyValueStore:
-    collection_name: str
-
-    def __init__(self, collection_name):
-        self.collection_name = collection_name
+    collection_name: str = 'simple_key_value_store'
 
     def get(self, key):
         raise NotImplementedError()
@@ -20,22 +14,28 @@ class SimpleKeyValueStore:
 
 
 class SimpleKeyValueStoreFirestore(SimpleKeyValueStore):
-    def __init__(self, collection_name=DEFAULT_COLLECTION_NAME):
-        super().__init__(collection_name)
+    def __init__(self):
         from firebase_admin import firestore
         self.kv = firestore.client().collection(self.collection_name)
 
     def get(self, key):
-        value = self.kv.document(self.collection_name).get().to_dict()[key]
+        value = self.kv.document(key).get().to_dict()
+        if key in value and len(value) == 1:
+            # Value is just a field
+            value = value[key]
         return value
 
     def set(self, key, value):
-        self.kv.document(self.collection_name).set({key: value})
+        if not isinstance(value, dict):
+            # Value is just a field
+            self.kv.document(key).set({key: value})
+        else:
+            # Value is a collection
+            self.kv.document(key).set(value)
 
 
 class SimpleKeyValueStoreLocal(SimpleKeyValueStore):
-    def __init__(self, collection_name=DEFAULT_COLLECTION_NAME):
-        super().__init__(collection_name)
+    def __init__(self):
         self.kv = {}
 
     def get(self, key):
