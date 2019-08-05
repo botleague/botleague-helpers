f90160b500c094d28ac59410ab54a5c1aa731ceb
Craig Quiter <cquiter@gmail.com>
2019-08-05 15:02:39 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(abspath(join(dirname(__file__), 'README.md')), 'rb') as readme_file:
 
 setup(
     name='botleague-helpers',
-    version='0.0.5',
+    version='0.0.6',
     description='Helpers for Bot League',
     long_description=__readme__,
     long_description_content_type='text/markdown',
