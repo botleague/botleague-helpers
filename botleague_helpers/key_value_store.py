89e2683c0f3bfbf8d4594f0f00683c87836c3963
Craig Quiter <cquiter@gmail.com>
2019-08-05 15:02:30 -0700

    Add compare_and_swap (cas), box mode, namespaces, and test data deletion to kv store

--- botleague_helpers/key_value_store.py
+++ botleague_helpers/key_value_store.py
@@ -1,70 +1,205 @@
 from __future__ import print_function
 
+import random
+import string
+import sys
+from typing import Any
+
+from box import BoxList, Box
+
 from botleague_helpers.config import blconfig
 from botleague_helpers.config import get_test_name_from_callstack
+from google.cloud import firestore
 
 DEFAULT_COLLECTION = 'simple_key_value_store'
 
 
 class SimpleKeyValueStore:
+    db = None
+    collection = None
+
+    def __init__(self, collection_name, use_boxes):
+        self.collection_name = collection_name or DEFAULT_COLLECTION
+        self.use_boxes = use_boxes
+
     def get(self, key):
-        raise NotImplementedError()
+        ret = self._get(key)
+        ret = self._to_box(ret)
+        return ret
 
     def set(self, key, value):
+        value = self._from_box(value)
+        self._set(key, value)
+
+    def compare_and_swap(self, key, expected_current_value, new_value) -> bool:
+        """
+        Atomically update the key to the new value if the current value is the
+        expected value.
+        https://en.wikipedia.org/wiki/Compare-and-swap
+        """
+        new_value = self._from_box(new_value)
+        expected_current_value = self._from_box(expected_current_value)
+        return self._compare_and_swap(key, expected_current_value, new_value)
+
+    def delete_all_test_data(self):
         raise NotImplementedError()
+
+    def _compare_and_swap(self, key, expected_current_value, new_value) -> bool:
+        raise NotImplementedError()
+
+    def _get(self, key):
+        raise NotImplementedError()
+
+    def _set(self, key, value):
+        raise NotImplementedError()
+
+    def _from_box(self, value):
+        if self.use_boxes:
+            if isinstance(value, BoxList):
+                value = value.to_list()
+            elif isinstance(value, Box):
+                value = value.to_dict()
+        return value
+
+    def _to_box(self, ret):
+        if self.use_boxes:
+            if isinstance(ret, list):
+                ret = BoxList(ret)
+            elif isinstance(ret, dict):
+                ret = Box(ret)
+        return ret
 
 
 class SimpleKeyValueStoreFirestore(SimpleKeyValueStore):
-    def __init__(self, collection_name):
+    def __init__(self, collection_name, use_boxes):
+        super().__init__(collection_name, use_boxes)
         from firebase_admin import firestore
         blconfig.ensure_firebase_initialized()
-        collection_name = collection_name or DEFAULT_COLLECTION
-        self.kv = firestore.client().collection(collection_name)
+        self.db = firestore.client()
+        self.collection = self.db.collection(self.collection_name)
 
-    def get(self, key):
-        value = self.kv.document(key).get().to_dict() or {}
-        if key in value and len(value) == 1:
+    def _get(self, key):
+        value = self.collection.document(key).get().to_dict() or {}
+        value = self._simplify_value(key, value)
+        return value
+
+    @staticmethod
+    def _simplify_value(key, value):
+        if value and key in value and len(value) == 1:
             # Document just contains the one field with the same name
             # as the document, so just return the value of the field.
             value = value[key]
         return value
 
-    def set(self, key, value):
+    def _set(self, key, value):
+        value = self._expand_value(key, value)
+        self.collection.document(key).set(value)
+
+    @staticmethod
+    def _expand_value(key, value) -> Any:
+        # Inverse of simplify value
         if not isinstance(value, dict):
             # Value is just a field
-            self.kv.document(key).set({key: value})
+            ret = {key: value}
         else:
             # Value is a collection
-            self.kv.document(key).set(value)
+            ret = value
+        return ret
+
+    def _compare_and_swap(self, key, expected_current_value, new_value) -> bool:
+        ref = self.collection.document(key)
+        transaction = self.db.transaction()
+
+        @firestore.transactional
+        def update_in_transaction(transaction_,
+                                  ref_, expected_current_value_,
+                                  new_value_) -> bool:
+            """
+            In the case of a concurrent edit, Cloud Firestore runs the
+            entire transaction again. In this case the expected value will
+            be different and set() will not be called, thus returning False.
+            """
+            snapshot = ref_.get(transaction=transaction_).to_dict()
+            snapshot = self._simplify_value(key, snapshot)
+            if snapshot == expected_current_value_:
+                transaction_.set(ref_, self._expand_value(key, new_value_))
+                ret_ = True
+            else:
+                ret_ = False
+            return ret_
+
+        ret = update_in_transaction(transaction, ref, expected_current_value,
+                                    new_value)
+        return ret
+
+    def delete_all_test_data(self):
+        if self.collection_name.startswith('test_'):
+            delete_firestore_collection(self.collection)
+            return True
+        else:
+            print('Not deleting collection %s whose name does not start with'
+                  ' test_', file=sys.stderr)
+            return False
+
+
+def delete_firestore_collection(coll_ref, batch_size=10):
+    docs = coll_ref.limit(batch_size).get()
+    deleted = 0
+
+    for doc in docs:
+        print(u'Deleting doc {} => {}'.format(doc.id, doc.to_dict()))
+        doc.reference.delete()
+        deleted = deleted + 1
+    if deleted >= batch_size:
+        return delete_collection(coll_ref, batch_size)
+
+
+LOCAL_COLLECTIONS = {}
 
 
 class SimpleKeyValueStoreLocal(SimpleKeyValueStore):
-    def __init__(self, collection_name):
-        self.kv = {}
+    def __init__(self, collection_name, use_boxes):
+        super().__init__(collection_name, use_boxes)
+        self.collection = LOCAL_COLLECTIONS.setdefault(collection_name, {})
 
-    def get(self, key):
-        return self.kv[key]
+    def _get(self, key):
+        return self.collection.get(key, None)
 
-    def set(self, key, value):
-        self.kv[key] = value
+    def _set(self, key, value):
+        self.collection[key] = value
+
+    def _compare_and_swap(self, key, expected_current_value, new_value) -> bool:
+        # Not threadsafe!
+        if self.collection[key] == expected_current_value:
+            self.collection[key] = new_value
+            return True
+        else:
+            return False
+
+    def delete_all_test_data(self):
+        keys = list(LOCAL_COLLECTIONS.keys())
+        for key in keys:
+            del LOCAL_COLLECTIONS[key]
 
 
 def get_key_value_store(
         collection_name: str = DEFAULT_COLLECTION,
-        test_remote_db=False) -> SimpleKeyValueStore:
+        force_firestore_db=False,
+        use_boxes=False) -> SimpleKeyValueStore:
     """
 
-    :param collection_name:
+    :param collection_name: Namespace for your kv store
     :param test_remote_db: For special cases where you want to test the db logic
+    :param use_boxes: Return python-box objects instead of dicts / lists
     :return:
     """
     test_name = get_test_name_from_callstack()
-    if test_name and not test_remote_db:
+    if test_name and not force_firestore_db:
         print('We are in a test, %s, so not using Firestore' % test_name)
-        return SimpleKeyValueStoreLocal(collection_name)
+        return SimpleKeyValueStoreLocal(collection_name, use_boxes)
     elif blconfig.should_use_firestore:
         print('Using Firestore backed key value store')
-        return SimpleKeyValueStoreFirestore(collection_name)
+        return SimpleKeyValueStoreFirestore(collection_name, use_boxes)
     else:
         print('SHOULD_USE_FIRESTORE is false, so not using Firestore')
-        return SimpleKeyValueStoreLocal(collection_name)
+        return SimpleKeyValueStoreLocal(collection_name, use_boxes)
--- botleague_helpers/test_live_db.py
+++ botleague_helpers/test_live_db.py
@@ -3,6 +3,16 @@ import string
 
 from botleague_helpers.key_value_store import get_key_value_store
 
+TEST_DB_NAME = 'test_db_delete_me'
+
+
+def test_compare_and_set_live_db():
+    kv = get_key_value_store(TEST_DB_NAME, force_firestore_db=True)
+    kv.set('yo', 1)
+    should_be_false = kv.compare_and_swap('yo', 2, 2)
+    assert should_be_false is False
+    assert kv.get('yo') == 1
+
 
 def test_namespace_live_db():
     rand_str_get_set(collection_name='')
@@ -11,7 +21,7 @@ def test_namespace_live_db():
 
 def rand_str_get_set(collection_name):
     kv = get_key_value_store(collection_name,
-                             test_remote_db=True)
+                             force_firestore_db=True)
     rand_str = 'test_data_can_delete_' + ''.join(
         random.choice(string.ascii_lowercase + string.digits)
         for _ in range(12))
@@ -20,4 +30,8 @@ def rand_str_get_set(collection_name):
 
 
 if __name__ == '__main__':
+    test_compare_and_set_live_db()
     test_namespace_live_db()
+    # watch_collection_play()
+    # TODO: Put test data in a separate project
+    # TODO: Clean things up, very carefully
