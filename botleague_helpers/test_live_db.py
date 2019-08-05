4da237047def0758cce997da28d5bef048d4effb
Craig Quiter <cquiter@gmail.com>
2019-08-05 15:02:46 -0700

    Playing around with watching collections, it works!

--- botleague_helpers/test_live_db.py
+++ botleague_helpers/test_live_db.py
@@ -16,7 +16,30 @@ def test_compare_and_set_live_db():
 
 def test_namespace_live_db():
     rand_str_get_set(collection_name='')
-    rand_str_get_set(collection_name='test_db')
+    rand_str_get_set(collection_name=TEST_DB_NAME)
+
+
+def watch_collection_play():
+    kv = get_key_value_store(TEST_DB_NAME, force_firestore_db=True)
+
+    # Create a callback on_snapshot function to capture changes
+    def on_snapshot(col_snapshot, changes, read_time):
+        for change in changes:
+            if change.type.name == 'ADDED':
+                print(u'New city: {}'.format(change.document.id))
+            elif change.type.name == 'MODIFIED':
+                print(u'Modified city: {}'.format(change.document.id))
+            elif change.type.name == 'REMOVED':
+                print(u'Removed city: {}'.format(change.document.id))
+
+    col_query = kv.collection.where('b', '>=', '')
+
+    # Watch the collection query
+    query_watch = col_query.on_snapshot(on_snapshot)
+
+    kv.set('a3', {'b': 'c'})
+    kv.set('a4', {'b': 'd'})
+    input('press any key to exit')
 
 
 def rand_str_get_set(collection_name):
