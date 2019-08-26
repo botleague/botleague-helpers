f7c549625f14c1979924ac30d032df57db2ba5a5
Craig Quiter <cquiter@gmail.com>
2019-08-26 16:20:38 -0700

    Support github dependencies in setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,29 @@ from setuptools import setup
 from botleague_helpers import VERSION
 
 
-def get_requires() -> List[str]:
+def get_requires() -> Tuple[List[str], List[str]]:
     with open('requirements.txt') as reqs_file:
-        reqs = reqs_file.read().split(os.sep)
-    return reqs
+        reqs = reqs_file.read().split(os.linesep)
+    dep_links = []
+    final_reqs = []
+    for req in reqs:
+        if not req.startswith('git+git://github.com/'):
+            final_reqs.append(req)
+        else:
+            req = req.replace('git+git', 'https')
+            url, egg = req.split('#egg=')
+            dep_link = url + '/tarball/master'
+            if egg:
+                dep_link += '#egg=' + egg
+            dep_links.append(dep_link)
+    return final_reqs, dep_links
 
 
 # Read the README markdown data from README.md
 with open(abspath(join(dirname(__file__), 'README.md')), 'rb') as readme_file:
     __readme__ = readme_file.read().decode('utf-8')
+
+requires, dependency_links = get_requires()
 
 setup(
     name='botleague-helpers',
@@ -36,5 +50,6 @@ setup(
     packages=['botleague_helpers'],
     zip_safe=True,
     python_requires='>=3.7',
-    install_requires=get_requires()
+    install_requires=requires,
+    dependency_links=dependency_links,
 )
